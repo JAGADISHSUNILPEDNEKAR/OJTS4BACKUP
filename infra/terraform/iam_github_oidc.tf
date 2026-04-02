4a05991e25d868efb6dc75362904f87d1056ff38
JagadishSunilPednekar <jsphere16@gmail.com>
2026-04-02 17:16:02 +0530

    chore(infra): add import blocks for OIDC role and provider

--- infra/terraform/iam_github_oidc.tf
+++ infra/terraform/iam_github_oidc.tf
@@ -1,8 +1,19 @@
-# GitHub Actions OIDC Provider
+# GitHub Actions OIDC Provider (Imported from manual bootstrapping)
+import {
+  to = aws_iam_openid_connect_provider.github
+  id = "arn:aws:iam::896170900409:oidc-provider/token.actions.githubusercontent.com"
+}
+
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"] # GitHub's OIDC certificate thumbprint
+}
+
+# IAM Role for GitHub Actions (Imported from manual bootstrapping)
+import {
+  to = aws_iam_role.github_actions_role
+  id = "github-actions-origin-role"
 }
 
 # IAM Role for GitHub Actions
